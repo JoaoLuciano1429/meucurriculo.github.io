ab655fc182c9c29602445d8971f33ed5d5180e90
João Luciano <104032132+JoaoLuciano1429@users.noreply.github.com>
2023-09-30 12:50:38 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function getAge(dateString) {
+  const today = new Date();
+  const birthDate = new Date(dateString);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age;
+}
+
+document.getElementById("ageDisplay").innerText = getAge("2003/09/06");
