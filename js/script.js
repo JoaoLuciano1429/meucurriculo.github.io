250bf0c58917eda1f93ea5db7d5c427e3fb0dd1b
João Luciano <104032132+JoaoLuciano1429@users.noreply.github.com>
2023-10-10 19:21:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,17 @@ function getAge(dateString) {
 }
 
 document.getElementById("ageDisplay").innerText = getAge("2003/09/06");
+
+const certBtn = document.getElementById("viewCert");
+const showingCert = document.querySelector("superbody");
+const closeBtn = document.getElementById("closeCert");
+
+certBtn.addEventListener("click", () => {
+  showingCert.classList.add("showCert");
+  closeBtn.classList.add("showCert");
+});
+
+closeBtn.addEventListener("click", () => {
+  showingCert.classList.remove("showCert");
+  closeBtn.classList.remove("showCert");
+});
